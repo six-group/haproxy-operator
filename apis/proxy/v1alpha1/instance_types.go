34809d5d842974a31f067a820859df9a39d6de5b
Iker Camacho <iker.camacho@onum.com>
2024-08-06 16:27:19 +0200

    Remove redundant comment

--- apis/proxy/v1alpha1/instance_types.go
+++ apis/proxy/v1alpha1/instance_types.go
@@ -100,7 +100,7 @@ type RouteSpec struct {
 type ServiceSpec struct {
 	// Enabled will toggle the creation of a Service.
 	Enabled bool `json:"enabled"`
-	// Type will define the service type, defaults to ClusterIP
+	// Type will define the service type
 	// +optional
 	// +kubebuilder:validation:Enum=ClusterIP;NodePort;LoadBalancer
 	// +kubebuilder:default=ClusterIP
